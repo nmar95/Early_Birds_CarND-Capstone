aa37f9cd4b07cf0100a4a94d66347a89ddcff186
Arjaan Buijk <arjaan.buijk@gmail.com>
2017-10-31 09:58:24 -0400

    Initial implementation of waypoint_updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+AB_DEBUG = False
+
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 
 import math
+import numpy as np
+from scipy.spatial import distance
+from itertools import islice, cycle
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -37,16 +42,111 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.base     = None   # base waypoints
+        self.final    = None   # final waypoints
+        self.final_i1 = None   # index of first final waypoint in base waypoints
 
         rospy.spin()
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        if AB_DEBUG:
+            rospy.logwarn("===========================================================\n \
+                           Entered callback pose_cb \n")
+            #rospy.logwarn("W0: msg = %s",msg)
+        
+        if self.base is None:
+            return  # don't have base yet. 
+        
+        #
+        # Logic:
+        # - Find the closest waypoint to car p1
+        # - Next waypoint p2 from list of waypoints
+        # - Calculate dot product of these vectors:
+        #     v1: p1->p2
+        #     v2: p1->car
+        # - If dot product is positive then p2 is next waypoint else p1
+        
+        car_pos = np.array( [[msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]] ) # 2D array, valid for cdist
+        dist    = distance.cdist(self.base_pos, car_pos)
+        i1      = np.argmin(dist)
+        if i1 == len(self.base_pos)-1:
+            i2 = 0
+        else:
+            i2 = i1+1      
+        car_pos = car_pos.flatten() # get rid of 2nd dimension
+        
+        p1 = self.base_pos[i1]
+        p2 = self.base_pos[i2]    
+        v1 = p2      - p1
+        v2 = car_pos - p1
+        dot_product = np.dot(v1,v2)
+        
+        if dot_product < 0:
+            self.final_i1 = i1
+        else:
+            self.final_i1 = i2
+        
+        final_waypoints = list(islice(cycle(self.base.waypoints), self.final_i1, self.final_i1 + LOOKAHEAD_WPS - 1))
+        
+        velocity = 5.0 # for testing
+        for waypoint in final_waypoints:
+            waypoint.twist.twist.linear.x = velocity
+            
+        lane = Lane()
+        lane.waypoints = final_waypoints
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+        self.final_waypoints_pub.publish(lane)
+        
+        self.final = lane
+            
+            
+        
+        if AB_DEBUG: 
+            rospy.logwarn("Leaving callback pose_cb \n \
+                           ===========================================================\n")        
 
-    def waypoints_cb(self, waypoints):
+    def waypoints_cb(self, msg):
         # TODO: Implement
-        pass
+        """
+        Called once: provides waypoints of base
+        
+        The msg contains a styx_msgs/Lane for all the waypoints of the base.
+        """
+        if AB_DEBUG:        
+            rospy.logwarn("===========================================================\n \
+                           Entering callback waypoints_cb \n") 
+            rospy.logwarn("# of waypoints = %s",len(msg.waypoints))
+        
+        # store waypoints data in numpy arrays
+        base_pos = []     # Position:                  x, y, z
+        base_ori = []     # Orientation:               x, y, z, w
+        base_vel = []     # Velocity in car direction: x, y, z  (only x non-zero)
+        base_ang = []     # Angular velocity:          x, y, z
+        for wp in msg.waypoints:
+            wp_pos = [wp.pose.pose.position.x, wp.pose.pose.position.y, wp.pose.pose.position.z]
+            wp_ori = [wp.pose.pose.orientation.x, wp.pose.pose.orientation.y, wp.pose.pose.orientation.z, wp.pose.pose.orientation.w]
+            wp_vel = [wp.twist.twist.linear.x, wp.twist.twist.linear.y, wp.twist.twist.linear.z]
+            wp_ang = [wp.twist.twist.angular.x, wp.twist.twist.angular.y, wp.twist.twist.angular.z]
+            
+            base_pos.append(wp_pos)
+            base_ori.append(wp_ori)
+            base_vel.append(wp_vel)
+            base_ang.append(wp_ang)
+            
+        self.base_pos = np.array(base_pos)
+        self.base_ori = np.array(base_ori)
+        self.base_vel = np.array(base_vel)
+        self.base_ang = np.array(base_ang)
+        
+        # store base_waypoints
+        self.base = msg
+        
+        if AB_DEBUG:   
+            rospy.logwarn("Leaving callback waypoints_cb \n \
+                           ===========================================================\n") 
+                  
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
