fb7a6bd401813f5c6f308ce22769df5af8ae596a
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2017-11-21 23:30:08 +0300

    Car now moving smoothly on road.
    Car is now able to stop perfectly near the traffic signal light (if red).

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,112 +1,51 @@
-import rospy
 from pid import PID
 from yaw_controller import YawController
-
-DEBUG = False              # get printout
-
+import math
+import rospy
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, wheel_base, steer_ratio, min_speed, 
-                       max_lat_accel, max_steer_angle,
-                       vehicle_mass, wheel_radius, decel_limit, brake_deadband):
-        
-        # controller for throttle       
-        #self.tc = PID(0.1, 0.002, 0.0, mx=1.0) # kp, ki, kd as taken from Term 1 pid project
-        self.tc = PID(0.1, 0.1, 0.1, mx=1.0) # kp, ki, kd
-        
-        # controller for steer
-        self.yc = YawController(wheel_base, steer_ratio, min_speed, 
-                                max_lat_accel, 
-                                max_steer_angle)
-        
-        # values for brake torque calculation
-        self.vehicle_mass   = vehicle_mass
-        self.wheel_radius   = wheel_radius
-        self.decel_limit    = decel_limit
-        self.brake_deadband = brake_deadband
-        
-        # just for testing
-        self.test_0_status = 0
-        self.test_0_speed  = 5
+    def __init__(self, *args, **kwargs):
 
-    def reset_pid(self):
-        """
-        Reset the PID controllers
-        This is needed if dbw node is over-ruled by safety driver.
-        """
-        self.tc.reset()
-                
-    def control(self, 
-                target_linear_velocity, target_angular_velocity,
-                current_linear_velocity, current_angular_velocity,
-                sample_time):
+        self.vehicle_mass = kwargs['vehicle_mass']
+        self.fuel_capacity = kwargs['fuel_capacity']
+        self.brake_deadband = kwargs['brake_deadband']
+        self.decel_limit = kwargs['decel_limit']
+        self.accel_limit = kwargs['accel_limit']
+        self.wheel_radius = kwargs['wheel_radius']
+        self.wheel_base = kwargs['wheel_base']
+        self.steer_ratio = kwargs['steer_ratio']
+        self.max_lat_accel = kwargs['max_lat_accel']
+        self.max_steer_angle = kwargs['max_steer_angle']
 
-        throttle = 0.0
-        brake    = 0.0
-        steer    = 0.0
+        min_speed = 0
         
-        error = target_linear_velocity - current_linear_velocity
-        if error < 0.0:
-            # Need to brake - unit of torque (N*m)
-            decel = error/sample_time
-            brake = self.vehicle_mass * decel * self.wheel_radius  # Note: decel is negative            
-            
-        else:    
-            if sample_time > 0.0:
-                thc   = self.tc.step(error, sample_time)
-                if thc > 0:
-                    throttle = thc
+        self.velocity_pid = PID(kp=0.8, ki=0, kd=0.05, mn=self.decel_limit, mx=0.5 * self.accel_limit)
+        self.steering_pid = PID(kp=0.16, ki=0.001, kd=0.1, mn=-self.max_steer_angle, mx=self.max_steer_angle)
+        self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, min_speed, self.max_lat_accel, self.max_steer_angle)
         
-        if DEBUG:
-            rospy.logwarn("twist_controller: target_v, current_v, sample_time, throttle, brake, steer = %e, %e, %e %e, %e, %e",
-                          target_linear_velocity, current_linear_velocity, sample_time, throttle, brake, steer)
-               
-            
-        steer = self.yc.get_steering(target_linear_velocity,
-                                     target_angular_velocity,
-                                     current_linear_velocity)
-        
-        return throttle, brake, steer
 
-    def control_test(self, 
-                target_linear_velocity, target_angular_velocity,
-                current_linear_velocity, current_angular_velocity,
-                sample_time,
-                test=0):
-        """
-        Function to test speeding up & braking
-        """
-        
-        throttle = 0.0
-        brake    = 0.0
-        steer    = 0.0
+    def reset(self):
+        self.velocity_pid.reset()
+        self.steering_pid.reset()
 
-        if test == 0:
-            # Speed up until certain speed is reached
-            # Then brake until stopped
-            
-            if self.test_0_status == 0:
-                # speed up till speed reached
-                if current_linear_velocity <= self.test_0_speed:
-                    throttle = 1.0
-                else:
-                    self.test_0_status = 1
-                    
-            if self.test_0_status == 1:
-                # break till stop
-                if current_linear_velocity > 1.E-6:  # never really reaches 0.0...??...
-                    brake = -1.E15
-                else:
-                    self.test_0_status = 0   # speed back up             
+    def control(self, target_linear_velocity, target_angular_velocity, current_linear_velocity, cross_track_error, sample_time):
+        linear_velocity_error = target_linear_velocity - current_linear_velocity
 
-            if DEBUG:
-                rospy.logwarn("twist_controller speed, throttle,brake= %e, %f, %f",current_linear_velocity,throttle,brake)
-    
-        steer = self.yc.get_steering(target_linear_velocity,
-                                     target_angular_velocity,
-                                     current_linear_velocity)
+        velocity_correction = self.velocity_pid.step(linear_velocity_error, sample_time)
 
-        return throttle, brake, steer
+        brake = 0
+        throttle = velocity_correction
+
+        if(throttle < 0):
+            deceleration = abs(throttle)
+            brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius * deceleration if deceleration > self.brake_deadband else 0.
+            throttle = 0
+
+        predictive_steering = self.yaw_controller.get_steering(target_linear_velocity, target_angular_velocity, current_linear_velocity)
+        # without Steering PID car will wiggle around the waypoint path
+        corrective_steering = self.steering_pid.step(cross_track_error, sample_time)
+        steering = predictive_steering + corrective_steering
+        return throttle, brake, steering
