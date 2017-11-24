de7b69da070cb752eae064d666cb9bb024dfe73b
Andras Wienzek <andreas.wienzek@gmail.com>
2017-11-24 16:19:07 +0100

    comments

--- ros/run.sh
+++ ros/run.sh
@@ -1,11 +1,15 @@
+#enc
 source /opt/ros/kinetic/setup.bash
 
 #cd ros
 
+# clean
 rm -rf devel
 rm -rf build
 catkin_make clean
+rosclean
 
+# run
 catkin_make
 source devel/setup.sh
 roslaunch launch/styx.launch
