44803cf76a04b0a0807a5ceaa982da73e81064ee
Nick <nmar95@users.noreply.github.com>
2017-11-24 21:41:30 -0500

    Update twist_controller.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -17,9 +17,7 @@ class Controller(object):
         self.steer_ratio = kwargs['steer_ratio']
         self.max_lat_accel = kwargs['max_lat_accel']
         self.max_steer_angle = kwargs['max_steer_angle']
-
         min_speed = 0
-        
         self.velocity_pid = PID(kp=0.8, ki=0, kd=0.05, mn=self.decel_limit, mx=0.5 * self.accel_limit)
         self.steering_pid = PID(kp=0.16, ki=0.001, kd=0.1, mn=-self.max_steer_angle, mx=self.max_steer_angle)
         self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, min_speed, self.max_lat_accel, self.max_steer_angle)
@@ -30,12 +28,10 @@ class Controller(object):
 
     def control(self, target_linear_velocity, target_angular_velocity, current_linear_velocity, cross_track_error, sample_time):
         linear_velocity_error = target_linear_velocity - current_linear_velocity
-
         velocity_correction = self.velocity_pid.step(linear_velocity_error, sample_time)
-
         brake = 0
         throttle = velocity_correction
-
+        
         if(throttle < 0):
             deceleration = abs(throttle)
             brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius * deceleration if deceleration > self.brake_deadband else 0.
